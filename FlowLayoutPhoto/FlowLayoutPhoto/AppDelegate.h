a0ef762150182a55d6145a3c453efd7aab6876d4
apple <apple@appledeMBP-2.lan>
2017-09-07 22:15:09 +0800

    添加项目忽略文件

---
+++ FlowLayoutPhoto/FlowLayoutPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlowLayoutPhoto
+//
+//  Created by apple on 17/9/7.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlowLayoutPhoto/FlowLayoutPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlowLayoutPhoto
+//
+//  Created by apple on 17/9/7.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
